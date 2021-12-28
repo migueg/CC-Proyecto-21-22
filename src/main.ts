7493cfe6463b4377ed2461858c4dcceaa84ca074
Migue <e.miguetenorio@go.ugr.es>
2021-12-28 14:04:28 +0100

    #60; Renombro función que levanta el microservicio

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {logger} from './logger/logger';
-async function bootstrap() {
+async function up() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('workwith-api/v1'); 
   await app.listen(3000);
 }
 logger.info('Iniciando app')
-bootstrap();
+up();
 logger.info('App iniciada en el puerto 3000');
