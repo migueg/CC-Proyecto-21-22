2082fd1191957bced7c6310a5de1357804b778e8
Migue <e.miguetenorio@go.ugr.es>
2022-01-12 21:43:39 +0100

    #60; Arreglo problema con configuracion que hacia que no se levantara el server y añado scripts para compilar y levantar el server

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import {Module} from '@nestjs/common';
 import {cvController} from "./controllers/cvController";
 import {CurriculumService} from "./services/curriculums.service";
 import { MulterModule } from '@nestjs/platform-express';
-import { offerController } from '../src/controllers/offer.controller';
-import {OfferService} from '../src/services/offer.service';
+import { offerController } from './controllers/offer.controller';
+import {OfferService} from './services/offer.service';
 
 @Module({
   imports: [MulterModule.register({
