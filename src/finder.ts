801f196e9fab3ce7a83c3f8c9169fc3a02fafb6a
Migue <e.miguetenorio@go.ugr.es>
2022-01-12 19:55:59 +0100

    #40; Añado los metodos que contendran la funcionalidad para buscar ofertas afines

--- src/finder.ts
+++ src/finder.ts
@@ -1,6 +1,32 @@
+import { FindOfferDto, FindOfferResponseDto } from './dtos/find-offer.dto';
 export class Finder {
     // to implement
     public fetchAPI(url : string , params : object) : object{
         return {};
+   }
+
+   public find(findOfferDto : FindOfferDto) : FindOfferResponseDto{
+    let response : FindOfferResponseDto;
+
+    response = new FindOfferResponseDto();
+
+    response.status = 'Not implemented'
+    response.offers = [];
+    response.id = "";
+
+    return response;
+   }
+
+   public getOffers(id : string) : FindOfferResponseDto{
+
+    let response : FindOfferResponseDto;
+
+    response = new FindOfferResponseDto();
+
+    response.status = 'Not implemented'
+    response.offers = [];
+    response.id = "";
+
+    return response;
    }
 }
