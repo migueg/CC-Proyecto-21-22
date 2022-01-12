528555dbf419ec2e94664bb28cb09e8734f188cb
Migue <e.miguetenorio@go.ugr.es>
2022-01-12 19:33:26 +0100

    #60; Escribo el resto de test que testearan los endpoints de la API

--- test/api.test.ts
+++ test/api.test.ts
@@ -4,7 +4,11 @@ const mocks = require('node-mocks-http');
 const converterToJSON =  new Converter.ConverterToJson(["spanish","english"], "../test/mocks/curriculum.pdf");
 import { cvController } from '../src/controllers/cvController';
 import {CurriculumService} from '../src/services/curriculums.service';
+import { offerController } from '../src/controllers/offer.controller';
+import {OfferService} from '../src/services/offer.service';
 import {Test} from '@nestjs/testing';
+import { FindOfferDto } from '../src/dtos/find-offer.dto';
+import { ApplyOfferDto } from '../src/dtos/apply-offer.dto';
 
 var streamBuffers = require('stream-buffers');
 
@@ -121,5 +125,178 @@ describe('Quiero utilizar la api del sistema',() => {
             })
         })
        
+    });
+    describe('Necesito actuar sobre el recurso oferta', ()=>{
+
+        let offerControl: offerController
+        let offerService : OfferService
+
+        beforeEach(async () =>{
+            const moduleTest = await Test.createTestingModule({
+                controllers: [offerController],
+                providers: [OfferService],
+            }).compile()
+        
+
+            offerControl = moduleTest.get<offerController>(offerController);
+            offerService = moduleTest.get<OfferService>(OfferService);
+        })
+
+        let findOfferDto : FindOfferDto = new FindOfferDto();
+
+            findOfferDto.age = 20;
+            findOfferDto.competences = [{
+                technical: ["c++", "java"],
+                personal: ["team work", "scrum"]
+            }]
+            findOfferDto.education = [{
+                name: "Ingenieria Informática",
+                location: "Granada",
+                institution: "UGR",
+                initialDate: "2017-09-09",
+                endDate: "2021-07-31"
+            }]
+
+            findOfferDto.experience = [{
+                job: "Técnico redes",
+                company: "Redelan",
+                place: "Armilla",
+                startDate: "2020-03-01",
+                endDate: "2021-03-01",
+                description: "trabajo con redes"
+            }]
+
+            findOfferDto.gender = "male"
+            findOfferDto.languages = [{
+                language: "ingles",
+                writing: "alto",
+                speaking: "alto",
+                listening: "alto",
+            }]
+
+            findOfferDto.location = "España"
+            findOfferDto.range = 100
+            
+        describe('Find offers', ()=>{
+            
+            test('Si solicito ofertas afines de manera adecuada obtengo las ofertas',()=>{
+                const req = mocks.createRequest();
+                req.res = mocks.createResponse();
+                
+                req.headers['Content-Type'] = 'application/json';
+               
+                var res = offerControl.find(findOfferDto,req,req.res)
+
+                expect(res._getStatusCode()).toEqual(200);
+
+            })
+
+            test('Si solicito ofertas afines con un content-type erroneo obtengo códgio de error', ()=>{
+                const req = mocks.createRequest();
+                req.res = mocks.createResponse();
+                
+                req.headers['Content-Type'] = 'application/xml';
+               
+                var res = offerControl.find(findOfferDto,req,req.res)
+
+                expect(res._getStatusCode()).toEqual(400);
+
+            })
+            test('Si solicito ofertas afines sin especificar rango ni localización obtengo código de error', ()=>{
+                const req = mocks.createRequest();
+                req.res = mocks.createResponse();
+                
+                req.headers['Content-Type'] = 'application/json';
+                
+                findOfferDto.range = null;
+                var res = offerControl.find(findOfferDto,req,req.res)
+
+                expect(res._getStatusCode()).toEqual(400);
+
+                findOfferDto.range = 200
+                findOfferDto.location = null
+                
+
+                res = offerControl.find(findOfferDto,req,req.res)
+
+                expect(res._getStatusCode()).toEqual(400);
+            })
+            test('Si solicito ofertas afines sin información suficiente para buscar obtengo código de error', ()=>{
+                const req = mocks.createRequest();
+                req.res = mocks.createResponse();
+                
+                req.headers['Content-Type'] = 'application/json';
+                
+                findOfferDto.location = "Armilla"
+                findOfferDto.experience = undefined;
+                findOfferDto.education = undefined;
+                findOfferDto.competences = undefined;
+
+                var res = offerControl.find(findOfferDto,req,req.res)
+
+                expect(res._getStatusCode()).toEqual(400);
+            })
+        })
+        describe('Get offers', () => {
+            test('Si pido las ofertas para un usuario que existe y que solicito ofertas recibo las ofertas', () => {
+                const req = mocks.createRequest();
+                req.res = mocks.createResponse();
+
+                var res = offerControl.getOffers('1',req,req.res);
+                expect(res._getStatusCode()).toEqual(200);
+            })
+
+            //More test will be implemented
+        });
+
+        describe('Get afinity', () => {
+            test('Si pido la afinidad para una oferta existente obtengo un codigo 200', ()=>{
+                const req = mocks.createRequest();
+                req.res = mocks.createResponse();
+
+                var res = offerControl.getAfinity('1',req,req.res);
+                expect(res._getStatusCode()).toEqual(200);
+            })
+
+            //More test will be implemented
+        })
+
+        describe('Apply offer', () => {
+            let applyOfferDto : ApplyOfferDto  = new ApplyOfferDto();
+            applyOfferDto.id = '1';
+            applyOfferDto.email = "test@gmail.com";
+            applyOfferDto.name = "pepe";
+            test('Si solicito una oferta existente con el body correcto y el content-type correcto obtengo código 200', ()=>{
+                const req = mocks.createRequest();
+                req.res = mocks.createResponse();
+                
+                req.headers['Content-Type'] = 'application/json'
+
+                var res = offerControl.applyOffer(applyOfferDto,req,req.res);
+                expect(res._getStatusCode()).toEqual(200);
+            })
+
+            test('Si solicito una oferta con un content-type erroneo obtengo un código 400', () =>{
+                const req = mocks.createRequest();
+                req.res = mocks.createResponse();
+
+                req.headers['Content-Type'] = 'application/text'
+
+                var res = offerControl.applyOffer(applyOfferDto,req,req.res);
+                expect(res._getStatusCode()).toEqual(400);
+            })
+
+            test('Si solicito una oferta con un el body incorrecto obtengo un código 400', () =>{
+                const req = mocks.createRequest();
+                req.res = mocks.createResponse();
+
+                req.headers['Content-Type'] = 'application/json'
+                
+                applyOfferDto.id = ""
+
+                var res = offerControl.applyOffer(applyOfferDto,req,req.res);
+                expect(res._getStatusCode()).toEqual(400);
+            })
+        })
     })
-});
+})
