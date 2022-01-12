a0e6bae4b4644bb9e39a5c19ba5f04ecd3bf3629
Migue <e.miguetenorio@go.ugr.es>
2022-01-12 19:50:58 +0100

    #60; Incluyo en el router principal de la app el controlador para las offertas y el servicio

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import {Module} from '@nestjs/common';
 import {cvController} from "./controllers/cvController";
 import {CurriculumService} from "./services/curriculums.service";
 import { MulterModule } from '@nestjs/platform-express';
+import { offerController } from '../src/controllers/offer.controller';
+import {OfferService} from '../src/services/offer.service';
 
 @Module({
   imports: [MulterModule.register({
       dest: './files', //La carpeta de destino empieza en el root del proyecto
   })],
-  controllers: [cvController], 
-  providers: [CurriculumService], 
+  controllers: [cvController,offerController], 
+  providers: [CurriculumService, OfferService], 
 })
 export class AppModule {}
